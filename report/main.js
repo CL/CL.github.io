ff0b838dad5771fc252696782647e9412d2b1039
Felipe Chagas <felipe.rabello@mrv.com.br>
2020-02-11 15:26:20 -0300

    Gerador de Report

---
+++ report/main.js
@@ -0,0 +1,19 @@
+$("#login-button").click(function(event){
+    debugger;
+    event.preventDefault();
+    let queryId = $("#query-id").text
+    $('form').fadeOut(500);
+    $.get(
+        "https://mrv-sharedfunction-dev.azurewebsites.net/api/CicleTimeReport?queryId="+queryId,
+        function(data) {
+            $('.wrapper').addClass('form-success');
+            window.location.href = data;
+        }
+    );
+}); 
+
+function download()
+{
+    let queryId = document.getElementById('query-id').textContent;
+    window.open('https://mrv-sharedfunction-dev.azurewebsites.net/api/CicleTimeReport?queryId='+queryId);
+}
